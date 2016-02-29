9f7fa394f6c32bf4457ac0ad72636a9901c2b5ed
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2016-02-29 11:47:41 +0900

    fix description text

--- jj_menu/__init__.py
+++ jj_menu/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-__author__  = 'ytyng'
-__version__ = '0.1.0'
-__license__ = 'MIT'
+__author__ = 'ytyng'
+__version__ = '0.1.1'
+__license__ = 'BSD'
--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ from jj_menu import __author__, __version__, __license__
 setup(
     name='jj-menu',
     version=__version__,
-    description='Simple CLI Menu',
+    description='Simple CUI (TUI) Launcher menu',
     license=__license__,
     author=__author__,
     author_email='ytyng@live.jp',
     url='https://github.com/jytyng/jj-menu.git',
-    keywords='CLI Menu, Python',
+    keywords='CUI, TUI, Launcher, Python, curses',
     packages=find_packages(),
     install_requires=['six'],
     entry_points={
