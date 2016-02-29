2668ae7d36a56b4f3e9c69376c9f569427430c68
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2016-02-29 17:25:40 +0900

    delete comment

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@
 from setuptools import setup, find_packages
 from jj_menu import __author__, __version__, __license__
 
-# In [2]: from setuptools.command.bdist_egg import _get_purelib
-#
-# In [3]: _get_purelib()
-# Out[3]: '/Users/yotsuyanagi/.virtualenvs/default/lib/python2.7/site-packages'
-# $ cd $(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"
-
 setup(
     name='jj-menu',
     version=__version__,
