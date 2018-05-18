e82a5a13cf4c1ad6fcf260e4c998eb69b1305b22
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2018-05-18 20:04:18 +0900

    activate script support zsh

--- activate-jj.sh
+++ activate-jj.sh
@@ -3,7 +3,7 @@ function jj(){
     RESULT_FILE=/tmp/_jj_result
     jj-menu --result-file=${RESULT_FILE}
     if [ $? = 0 ]; then
-        history -s `cat ${RESULT_FILE}`
+        history -s `cat ${RESULT_FILE}` 2>/dev/null
         source ${RESULT_FILE}
     fi
 }
